bc7a51f59a841ecdd02c81f84dd2a4a74b78ab91
funZX <asiminciuc@yahoo.com>
2020-03-29 15:04:59 +0300

    drawable resize to imgui window content size

--- sdk/sources/render/sim_drawable.cpp
+++ sdk/sources/render/sim_drawable.cpp
@@ -144,8 +144,15 @@ void CDrawable::Render( CDriver *driver )
 	SIM_ASSERT( m_material != nullptr );
 	SIM_ASSERT( m_material->GetEffect() != nullptr );
 
-	ImVec2 size = ImGui::GetWindowSize();
-	ImGui::Image((void*)m_material, ImVec2(size.x, size.y), ImVec2(0, 1), ImVec2(1, 0));
+    ImVec2 vMin = ImGui::GetWindowContentRegionMin();
+    ImVec2 vMax = ImGui::GetWindowContentRegionMax();
+
+    vMin.x += ImGui::GetWindowPos().x;
+    vMin.y += ImGui::GetWindowPos().y;
+    vMax.x += ImGui::GetWindowPos().x;
+    vMax.y += ImGui::GetWindowPos().y;
+
+	ImGui::Image((void*)m_material, ImVec2(vMax.x - vMin.x, vMax.y - vMin.y), ImVec2(0, 1), ImVec2(1, 0));
 }
 // ----------------------------------------------------------------------//
 }; // namespace rnr
